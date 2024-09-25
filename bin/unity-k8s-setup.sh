a55cf99cf1afa31366e386670f7b56048c133d81
Timothy C. Arland <tcarland@gmail.com>
2024-09-25 13:23:48 -0600

    sync

--- bin/unity-k8s-setup.sh
+++ bin/unity-k8s-setup.sh
@@ -4,7 +4,7 @@
 #
 binpath=$(dirname "$0")
 setupdir=$(dirname "$(realpath "$binpath")")
-version="v24.09.26"
+version="v24.09.26-uc2.0.2"
 
 tmpl="conf/unity-configmap-template.yaml"
 cfg="manifests/base/unity-configmap.yaml"
