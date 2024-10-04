ef3da807ec5d300de0a777a92ea7ec2c12ab94f8
Timothy C. Arland <tcarland@users.noreply.github.com>
2024-10-04 10:06:16 -0700

    Tca 2409.26 (#3)
    * add example overlay
    * update env template

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Unity Catalog on K8s
 While the Unity Catalog project is working on a Container build, there 
 are currently some issues with the build process from that repository. 
 
-Wanting to build out a deployment for Unity on Kubernetes, this repo 
-was created to track the manifests and a workable container build process 
-for Unity Catalog.
+Wanting to build out a deployment for testing Unity on Kubernetes, this 
+repository was created to track the manifests and a *workable* container 
+build for Unity Catalog.
 
 
 ## Deployment
 
-We do not create the namespace via the manifests as the deployment needs
+The namespace is not created via the manifests as the deployment needs
 a database (MySQL or Postgres) configured via the *ConfigMap* template.
 The deployment of this database is not currently covered by this repo and 
 there is not a strict requirement that said database lives in the 
@@ -22,6 +22,7 @@ Run the setup script
 ```sh
 export UNITY_MYSQL_HOST="mysql-service.unity.svc.cluster.local:3306"
 export UNITY_MYSQL_DB="ucdb"
+export UNITY_S3_BUCKET="s3a://host/bucket"
 export S3_ACCESS_KEY="myaccesskey"
 export S3_SECRET_KEY="mysecretkey"
 ./bin/unity-k8s-setup.sh
--- bin/unity-k8s-setup.sh
+++ bin/unity-k8s-setup.sh
@@ -2,39 +2,41 @@
 #
 # create the configmap from env secrets
 #
+pname=${0##\/*}
 binpath=$(dirname "$0")
 setupdir=$(dirname "$(realpath "$binpath")")
-version="v24.09.25"
+version="v24.09.26-uc2.0.2"
 
 tmpl="conf/unity-configmap-template.yaml"
 cfg="manifests/base/unity-configmap.yaml"
 
-UNITY_MYSQL_HOST="${UNITY_MYSQL_HOST:-mysql-service.unity.svc.cluster.local:3306}"
-UNITY_MYSQL_DB="${UNITY_MYSQL_DB:-ucdb}"
-UNITY_MYSQL_USER="${UNITY_MYSQL_USER:-uc_admin}"
-UNITY_MYSQL_PASSWORD="${UNITY_MYSQL_PASSWORD}"
-UNITY_S3_LOCATION="${UNITY_S3_LOCATION:-unity/uc}"
+UNITY_DBHOST="${UNITY_DBHOST:-mysql-service.unity.svc.cluster.local:3306}"
+UNITY_DBNAME="${UNITY_DBNAME:-ucdb}"
+UNITY_DBUSER="${UNITY_DBUSER:-ucadmin}"
+UNITY_DBPASS="${UNITY_DBPASS}"
+UNITY_S3_BUCKET="${UNITY_S3_BUCKET:-s3://minio.minio.svc/unity/ucdb}"
 
+# ---------------------------------
 
 if [[ -z "$S3_ACCESS_KEY" || -z "$S3_SECRET_KEY" ]]; then
-    echo "Error, missing S3 credentials."
+    echo "$pname Error, missing S3 credentials."
     exit 1
 fi
-if [ -z "$UNITY_MYSQL_PASSWORD" ]; then
-    echo "Error, UNITY_MYSQL_PASSWORD is unset"
+if [ -z "$UNITY_DBPASS" ]; then
+    echo "$pname Error, UNITY_DBPASS is unset"
     exit 1
 fi
 cd "$setupdir"
 if [ $? -ne 0 ]; then
-    echo "Error with path permissions in 'cd $setupdir'"
+    echo "$pname Error with path permissions in 'cd $setupdir'"
     exit 2
 fi
 
-export UNITY_MYSQL_HOST
-export UNITY_MYSQL_DB
-export UNITY_MYSQL_USER
-export UNITY_MYSQL_PASSWORD
-export UNITY_S3_LOCATION
+export UNITY_DBHOST
+export UNITY_DBNAME
+export UNITY_DBUSER
+export UNITY_DBPASS
+export UNITY_S3_BUCKET
 
 echo " -> Creating ConfigMap from template '$cfg'"
 ( cat $tmpl | envsubst > $cfg )
